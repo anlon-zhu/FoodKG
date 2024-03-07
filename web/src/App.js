64c4a2b6cf812dbf08fef75086e590ad83299636
Anlon Zhu <zhu.anlon@gmail.com>
2024-03-07 21:28:31 +0000

    env

--- web/src/App.js
+++ web/src/App.js
@@ -7,7 +7,8 @@ import RecipeModal from './recipeModal';
 import TopRecipes from './TopRecipes';
 import generateColorMap from './colorMap';
 
-const BASE_URL = process.env.ENV_URL || "https://ingreedient-flask-api.onrender.com/";
+const BASE_URL = process.env.REACT_APP_ENV_URL || "https://ingreedient-flask-api.onrender.com/";
+console.log(BASE_URL)
 
 function App() {
   const [ingredients, setIngredients] = useState([]);
@@ -38,6 +39,7 @@ function App() {
     axios.post(BASE_URL + 'recipes/by-ingredients', { ingredientIds: newValue.map(ingredient => ingredient.id)})
       .then(response => {
           // Parse the string into an object
+        console.log(response)
         const recipeNodes = JSON.parse(response.data.recipeNodes);
         const ingredientNodes = JSON.parse(response.data.ingredientNodes);
         const links = response.data.links;
